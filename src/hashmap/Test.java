b5c4ff231e8df717de1c2831be9c9a372a8294c1
luoyang <luoyang@192.168.0.101>
2016-11-29 22:30:07 +0800

    sss

--- src/hashmap/Test.java
+++ src/hashmap/Test.java
@@ -1,36 +1,37 @@
 package hashmap;
 
-
 public class Test {
-	
-	static int init = 512;
-
-	public static void main(String[] args) {
-		new Test().chenyi2(init);
-	}
-	
-	//³ËÒÔ2
-	public void chenyi2(int n) {
-		if(n > 10000) {
-			System.out.print(" " + n);
-			chuyi2(n);
-			return;
-		}
-		
-		System.out.print(" " + n);
-		
-		chenyi2(2 * n);
-	}
-	
-	//³ýÒÔ2
-	public void chuyi2(int n) {
-		if(n == init) {
-			System.out.print(" " + n);
-			return;
-		}
-		if(n < 10000)
-		System.out.print(" " + n);
-		chuyi2(n/2);
-	}
-
+    
+    static int init = 512;
+    
+    public static void main(String[] args) {
+        new Test().chenyi2(init);
+        
+        System.out.println();
+    }
+    
+    //ï¿½ï¿½ï¿½ï¿½2
+    public void chenyi2(int n) {
+        if (n > 10000) {
+            System.out.print(" " + n);
+            chuyi2(n);
+            return;
+        }
+        
+        System.out.print(" " + n);
+        
+        chenyi2(2 * n);
+    }
+    
+    //ï¿½ï¿½ï¿½ï¿½2
+    public void chuyi2(int n) {
+        if (n == init) {
+            System.out.print(" " + n);
+            return;
+        }
+        if (n < 10000)
+            System.out.print(" " + n);
+        chuyi2(n / 2);
+    }
+    
 }
